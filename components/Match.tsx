f55e790f0f833ce5681a1405d8960e677d602bdf
ErnestC30 <chow_ernest06@hotmail.com>
2021-12-06 17:39:01 -0500

    Match component updated

--- components/Match.tsx
+++ components/Match.tsx
@@ -3,64 +3,166 @@ import { Accordion, AccordionDetails, AccordionSummary, Box, Grid, Typography} f
 import { theme } from '../theme'
 import { SummonerDTO } from '../interfaces'; 
 
-function Match({summonerData, matchData}: {summonerData: SummonerDTO, matchData: any}) {
-    /* Component for detailed information on a single match.  */
+function Match({
+  summonerData,
+  matchData,
+}: {
+  summonerData: SummonerDTO;
+  matchData: any;
+}) {
+  /* Component for detailed information on a single match.  */
 
-    const gameDuration = getGameTime(matchData.info.gameDuration)
-    const searchedSummonerData = getSearchedSummonerData(summonerData, matchData.info.participants)
-    const searchedSummonerItems = getSearchedSummonerItems(searchedSummonerData)
+  const gameDuration = getGameTime(matchData.info.gameDuration);
+  const searchedSummonerData = getSearchedSummonerData(
+    summonerData,
+    matchData.info.participants
+  );
+  const searchedSummonerItems = getSearchedSummonerItems(searchedSummonerData);
+  const teamOneParticipants = matchData.info.participants.slice(0, 5);
+  const teamTwoParticipants = matchData.info.participants.slice(5, 10);
 
-    return (
-        <>
-            <Accordion sx={{backgroundColor: searchedSummonerData.win ? 'win' : 'lose'}}> 
-                <AccordionSummary >
-                    <Grid container spacing={3}>
-                        <Grid item>
-                            <Typography>{gameDuration}</Typography>
-                            <Typography>{searchedSummonerData.win ? 'Victory' : 'Defeat'}</Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography>{searchedSummonerData.championName}</Typography>
-                            <img width='50px'
-                                 height='50px'
-                                 src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${searchedSummonerData.championName}.png`}/>
-                        </Grid>
-                        <Grid item>
-                            <Typography>
-                                {searchedSummonerData.kills} /&nbsp; 
-                                {searchedSummonerData.deaths} /&nbsp;
-                                {searchedSummonerData.assists}
-                            </Typography>
-                            <Typography>K/D/A: {((searchedSummonerData.kills + searchedSummonerData.assists) / searchedSummonerData.deaths).toFixed(2)}</Typography>
-                        </Grid>
-                        <Grid item sx={{display: 'flex'}}>
-                            {searchedSummonerItems.map((item, index) => {
-                                {/* Renders item images or empty placeholder. -> MIGHT NEED TO CHANGE TO GRID FORMAT */}
-                                return item != '0' ? <img key={index} 
-                                            width='30px'
-                                            height='30px'
-                                            src={`https://ddragon.leagueoflegends.com/cdn/11.23.1/img/item/${item}.png`}/>
-                                        : <Box key={index} sx={{display: 'inline', width: '30px', height: '30px', borderStyle: 'solid', borderColor: 'black'}}></Box>
-                            })}
-                        </Grid>
-                        <Grid item>
-                            {/* GRID TO DIVIDE PARTICIPANTS IN 2 GROUPS?*/}
-                            {matchData.info.participants.map((participant: any) => {
-                                return <Typography 
-                                        key={participant.puuid}
-                                        variant='body2'
-                                        >
-                                            {participant.summonerName}</Typography>
-                            })}
-                        </Grid>
-                    </Grid>
-                </AccordionSummary>
-                <AccordionDetails>
-                    <Typography>Hello Again</Typography>
-                </AccordionDetails>
-            </Accordion>
-        </>
-    )
+  return (
+    <>
+      <Accordion
+        sx={{
+          backgroundColor: searchedSummonerData.win ? "win" : "lose",
+          margin: "5px",
+          minWidth: "850px",
+        }}
+      >
+        <AccordionSummary>
+          <Grid container spacing={3} alignItems="center">
+            <Grid item xs>
+              <Typography variant="body2" align="center">
+                {gameDuration}
+              </Typography>
+              <Typography variant="body1" align="center">
+                {searchedSummonerData.win ? "Victory" : "Defeat"}
+              </Typography>
+            </Grid>
+            <Grid item xs>
+              <Grid container direction="column" alignItems="center">
+                <Grid item>
+                  <Typography>{searchedSummonerData.championName}</Typography>
+                </Grid>
+                <Grid item>
+                  <img
+                    width="50px"
+                    height="50px"
+                    src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${searchedSummonerData.championName}.png`}
+                  />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs>
+              <Grid container direction="column" alignItems="center">
+                <Grid item>
+                  <Typography>
+                    {searchedSummonerData.kills} /&nbsp;
+                    {searchedSummonerData.deaths} /&nbsp;
+                    {searchedSummonerData.assists}
+                  </Typography>
+                </Grid>
+                <Grid item>
+                  <Typography>
+                    K/D/A:{" "}
+                    {(
+                      (searchedSummonerData.kills +
+                        searchedSummonerData.assists) /
+                      searchedSummonerData.deaths
+                    ).toFixed(2)}
+                  </Typography>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs sx={{ display: "flex" }}>
+              {searchedSummonerItems.map((item, index) => {
+                {
+                  /* Renders item images or empty placeholder. -> MIGHT NEED TO CHANGE TO GRID FORMAT */
+                }
+                return item != "0" ? (
+                  <img
+                    key={index}
+                    width="30px"
+                    height="30px"
+                    src={`https://ddragon.leagueoflegends.com/cdn/11.23.1/img/item/${item}.png`}
+                  />
+                ) : (
+                  <Box
+                    key={index}
+                    sx={{
+                      display: "inline",
+                      width: "30px",
+                      height: "30px",
+                      borderStyle: "solid",
+                      opacity: "70%",
+                      borderColor: "#e3e3e3",
+                      backgroundColor: "#ededed",
+                    }}
+                  ></Box>
+                );
+              })}
+            </Grid>
+            <Grid item xs>
+              <Grid container direction="column" alignItems='right'>
+                {teamOneParticipants.map((participant: any) => {
+                  return (
+                    <>
+                      <Typography
+                        key={participant.puuid}
+                        variant="body2"
+                        sx={{
+                          verticalAlign: "center",
+                          display: "inline-flex",
+                          margin: "1px",
+                        }}
+                      >
+                        <img
+                          src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${participant.championName}.png`}
+                          height="20px"
+                          width="20px"
+                        />
+                        &nbsp;
+                        {displaySummonerName(participant.summonerName)}
+                      </Typography>
+                    </>
+                  );
+                })}
+              </Grid>
+            </Grid>
+            <Grid item xs>
+              <Grid container direction="column">
+                {teamTwoParticipants.map((participant: any) => {
+                  return (
+                    <Typography
+                      key={participant.puuid}
+                      variant="body2"
+                      sx={{
+                        verticalAlign: "center",
+                        display: "inline-flex",
+                        margin: "1px",
+                      }}
+                    >
+                      <img
+                        src={`http://ddragon.leagueoflegends.com/cdn/11.23.1/img/champion/${participant.championName}.png`}
+                        height="20px"
+                        width="20px"
+                      />
+                      &nbsp;
+                      {displaySummonerName(participant.summonerName)}
+                    </Typography>
+                  );
+                })}
+              </Grid>
+            </Grid>
+          </Grid>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>Hello Again</Typography>
+        </AccordionDetails>
+      </Accordion>
+    </>
+  );
 } 
 
 export default Match 
@@ -88,4 +190,14 @@ function getSearchedSummonerItems(searchedSummonerData: any): Array<string> {
         itemArray.push(searchedSummonerData[`item${i}`].toString())
     }
     return itemArray
+}
+
+function displaySummonerName(name: string): string {
+    /* Displays the user's name limiting the maximum length that can be displayed. */
+    const MAX_NAME_LENGTH = 8
+    if (name.length > MAX_NAME_LENGTH) {
+        return (name.substring(0, MAX_NAME_LENGTH) + '...')
+    } else {
+        return name
+    }
 }
